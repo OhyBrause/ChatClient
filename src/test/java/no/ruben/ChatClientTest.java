28166eabda8d7df900651538ae208408c03a2db8
ruben <rubencarlsen@gmail.com>
2021-09-14 13:38:16 +0200

    tests passing (testing maven)

--- src/test/java/no/ruben/ChatClientTest.java
+++ src/test/java/no/ruben/ChatClientTest.java
@@ -6,11 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChatClientTest {
     @Test
-    void testJunitFails() {
-        ChatClient cc = new ChatClient();
-        assertEquals(2,cc.i);
-    }
-    @Test
     void testJunitPass() {
         ChatClient cc = new ChatClient();
         assertEquals(1,cc.i);
