fac91252cbd2d2f65f911eea77c58c8dc370b18a
ruben <rubencarlsen@gmail.com>
2021-09-14 13:36:42 +0200

    trying to fix maven

--- src/test/java/no/ruben/ChatClientTest.java
+++ src/test/java/no/ruben/ChatClientTest.java
@@ -6,8 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChatClientTest {
     @Test
-    void testJunit() {
+    void testJunitFails() {
         ChatClient cc = new ChatClient();
         assertEquals(2,cc.i);
+    }
+    @Test
+    void testJunitPass() {
+        ChatClient cc = new ChatClient();
+        assertEquals(1,cc.i);
     }
 }
