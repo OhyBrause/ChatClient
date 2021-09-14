2b1dfee1190a46380c702587d1671b83a86f1834
ruben <rubencarlsen@gmail.com>
2021-09-14 14:34:43 +0200

    test test200Response 🟢 basic httpclient setup

--- src/main/java/no/ruben/ChatClient.java
+++ src/main/java/no/ruben/ChatClient.java
@@ -1,8 +1,37 @@
 package no.ruben;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ChatClient {
-    public int i = 1;
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    int statusCode;
+    public ChatClient (String host, String requestTarget, int port, String message) throws IOException {
+        Socket socket = new Socket(host, port);
+        String request = (
+            "GET " + requestTarget + " HTTP/1.1\r\n" +
+            "Host: " + host + "\r\n" +
+            "Connection: close\r\n" +
+            "Message: " + message + "\r\n" +
+            "\r\n"
+        );
+        socket.getOutputStream().write(request.getBytes());
+
+        String statusLine = readLine(socket);
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder response = new StringBuilder();
+        int c;
+        while((c = socket.getInputStream().read()) != '\r') {
+            response.append((char) c);
+        }
+        socket.getInputStream().read();
+        System.out.println(response);
+        return response.toString();
+    }
+
+    public int getResponseCode() {
+        return statusCode;
     }
 }
--- src/test/java/no/ruben/ChatClientTest.java
+++ src/test/java/no/ruben/ChatClientTest.java
@@ -2,12 +2,14 @@ package no.ruben;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChatClientTest {
     @Test
-    void testJunitPass() {
-        ChatClient cc = new ChatClient();
-        assertEquals(1,cc.i);
+    void test200Response() throws IOException {
+        ChatClient cc = new ChatClient("httpbin.org", "/html", 80, "");
+        assertEquals(200, cc.getResponseCode());
     }
 }
