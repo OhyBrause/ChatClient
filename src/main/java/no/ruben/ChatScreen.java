86bad69cdd9f7b818c0bdb66a47806f18f13c005
ruben <rubencarlsen@gmail.com>
2021-09-16 14:53:09 +0200

    checking connection

--- src/main/java/no/ruben/ChatScreen.java
+++ src/main/java/no/ruben/ChatScreen.java
@@ -10,60 +10,45 @@ public class ChatScreen {
     String lastMessage = "";
     String ownMessage = "";
     boolean firstRun = true;
+    Scanner s = new Scanner(System.in);
 
     public ChatScreen(String host) {
         this.host = host;
     }
-    Scanner s = new Scanner(System.in);
 
     public void run() throws IOException, InterruptedException {
         System.out.println("----------------------");
         System.out.println("### Client started ###");
         System.out.println("----------------------");
+        listenToServer();
+    }
 
-            System.out.println(lastMessage);
-            while(true) {
-                listenToServer();
-//                sendMessage();
-            }
-        }
-
-    private void sendMessage() throws IOException, InterruptedException {
-        System.out.println("### WRITE MESSAGE: ###");
-        String message = s.nextLine();
-        ownMessage = message;
+    private void sendMessage(String message) throws IOException, InterruptedException {
         ChatClient cc = new ChatClient(host, 10800, message);
     }
 
     public void listenToServer() throws IOException, InterruptedException {
         System.out.println("### LISTENING TO SERVER. PRESS [ENTER] TO WRITE MESSAGE ###");
-        boolean blankLine = true;
-        loop:
         while (true) {
-            int available;
-            while ((available = System.in.available()) == 0) {
 
-                ChatClient cc = new ChatClient("localhost", 10800, "retrieveLastMessage");
+            // Checking server for new messages every x seconds
+            int serverRequestInterval = 1;
+            ChatClient cc = new ChatClient("localhost", 10800, "retrieveLastMessage");
 
-                if (!lastMessage.equals(cc.getMessageBody()) &&
-                        !(ownMessage.equals(cc.getMessageBody())) &&
-                !firstRun) {
-                    lastMessage = cc.getMessageBody();
-                    System.out.println(lastMessage);
-                    firstRun = false;
-                }
-                TimeUnit.SECONDS.sleep(1);
+            if (firstRun) {
+                System.out.println("first run");
+//                firstRun = false;
+            } else if (
+                !lastMessage.equals(cc.getMessageBody()) &&
+                !ownMessage.equals(cc.getMessageBody())
+            ) {
+                System.out.println("else if");
+                lastMessage = cc.getMessageBody();
+                System.out.println(lastMessage);
             }
+            TimeUnit.SECONDS.sleep(serverRequestInterval);
 
-            do {
-                if (System.in.read() == '\n') {
-                    if (blankLine)
-                        break loop;
-                    blankLine = true;
-                } else {
-                    blankLine = false;
-                }
-            } while (--available > 0);
+            sendMessage(s.nextLine());
         }
 
     }
