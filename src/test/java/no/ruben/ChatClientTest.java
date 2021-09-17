a811470a780d9d102c43f0e23db5487c206895e0
ruben <rubencarlsen@gmail.com>
2021-09-17 08:42:37 +0200

    Removed test-cases. Need to make a proper test suite.

--- src/test/java/no/ruben/ChatClientTest.java
+++ src/test/java/no/ruben/ChatClientTest.java
@@ -7,24 +7,5 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChatClientTest {
-//    @Test
-//    void test200Response() throws IOException {
-//        ChatClient cc = new ChatClient("httpbin.org", 80, "");
-//        System.out.println(cc.getMessageBody());
-//        assertEquals(200, cc.getResponseCode());
-//    }
-//    @Test
-//    void testEchoResponseFromLocalServer() throws IOException {
-//        String message = "Halooo";
-//        ChatClient cc = new ChatClient("localhost", 10800, message);
-//        System.out.println(cc.getMessageBody());
-//        assertEquals(message, cc.getMessageBody());
-//    }
-//    @Test
-//    void testEchoResponseFromRemoteServer() throws IOException {
-//        String message = "Halooo";
-//        ChatClient cc = new ChatClient("84.212.217.38", 10800, message);
-//        System.out.println(cc.getMessageBody());
-//        assertEquals(message, cc.getMessageBody());
-//    }
+
 }
